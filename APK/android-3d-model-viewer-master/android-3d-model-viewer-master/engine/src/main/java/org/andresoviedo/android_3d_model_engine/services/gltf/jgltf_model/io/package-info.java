10ca2ec15481cd4b9e616c17b1ad225470803f2b
Devsign_MF <54878706+Lowvoice-git@users.noreply.github.com>
2020-07-22 16:07:52 +0900

    2020-07-22 reload

--- APK/android-3d-model-viewer-master/android-3d-model-viewer-master/engine/src/main/java/org/andresoviedo/android_3d_model_engine/services/gltf/jgltf_model/io/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Classes for reading and writing glTF data 
- */
-package org.andresoviedo.android_3d_model_engine.services.gltf.jgltf_model.io;
-
--- APK/android-3d-model-viewer-master/android-3d-model-viewer-master/engine/src/main/java/org/andresoviedo/android_3d_model_engine/services/gltf/jgltf_model/v1/gl/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Classes related to the KHR_technique_webgl extension, for rendering
- * a glTF asset with GL.
- */
-package org.andresoviedo.android_3d_model_engine.services.gltf.jgltf_model.v1.gl;
-

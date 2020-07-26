b70fad0fbfd48c495dd3608155bf1604aefad1cb
Haren <36858137+a101a78@users.noreply.github.com>
2020-07-26 18:49:59 +0900

    Init

---
+++ TDC/app/src/main/java/com/little_wizard/tdc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.little_wizard.tdc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

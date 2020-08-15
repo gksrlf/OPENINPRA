a9182a78a3825fe60050f9ebda746037b8a625a3
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2020-08-16 01:09:34 +0900

    .

---
+++ APK/android-3D-model-viewer-2.6.1/app/build/generated/source/buildConfig/debug/org/andresoviedo/dddmodel2/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.andresoviedo.dddmodel2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.andresoviedo.dddmodel2";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 23;
+  public static final String VERSION_NAME = "2.7.0";
+}
